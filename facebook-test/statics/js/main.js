c4f53eaf734b961bc258fc1624c4039c8a2fcf70
mon_sat <tnk@daijiten.info>
2011-04-08 13:05:36 +0900

    add post stream

--- facebook-test/statics/js/main.js
+++ facebook-test/statics/js/main.js
@@ -1,3 +1,9 @@
+var mbs = {
+	fbUrl: 'http://apps.facebook.com/social-service-test/',
+	gaUrl: 'http://ds-facebook-test.appspot.com/',
+	description: 'あなたのfacebook活用度を100点満点で診断します。'
+};
+
 (function() {
 	var e = document.createElement('script');
 	e.async = true;
@@ -31,9 +37,21 @@ window.fbAsyncInit = function() {
 			jQuery('#login_button').click(function() {
 				FB.login(function(response){
 					if (response.session) {
-						top.location.href = 'http://apps.facebook.com/social-service-test/';
+						FB.api('/me', function(response) {
+							// post
+							FB.api('/me/feed', 'post', {
+								message: response.name + 'さんが、ソーシャルサービス診断の実施をお勧めしています',
+								picture: mbs.gaUrl + 'img/facebook-test-image.png',
+								link: mbs.fbUrl,
+								description: mbs.description
+							}, function(response){
+								top.location.href = mbs.fbUrl;
+							});
+						});
 					}
-				});
+				},
+				{ perms : 'read_stream,publish_stream' }
+				);
 			});
 		}
 	});
